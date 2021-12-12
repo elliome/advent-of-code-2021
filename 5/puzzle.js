f372ab2e6bb82edf443f7263d29c6b253cb956cf
Elliot Emmerson <elliotluke.emmerson@gmail.com>
2021-12-12 00:36:26 +0000

    day 5

---
+++ 5/puzzle.js
@@ -0,0 +1,71 @@
+const fs = require("fs");
+let puzzleData = fs.readFileSync("./input.txt").toString().split("\n");
+
+puzzleData = puzzleData.map((line) => {
+    const xy = line.split(" -> ");
+    const points = [];
+
+    xy.forEach((point) => {
+        const vector = point.split(",");
+        points.push({
+            x: parseInt(vector[0]),
+            y: parseInt(vector[1]),
+        });
+    });
+
+    return points;
+});
+
+const grid = [];
+
+for (let i = 0; i < 1000; i++) {
+    let temp = [];
+
+    for (let j = 0; j < 1000; j++) {
+        temp.push(0);
+    }
+
+    grid.push(temp);
+}
+
+puzzleData.forEach((line) => {
+    const [start, end] = line;
+
+    if (start.x != end.x && start.y != end.y) {
+        return;
+    }
+
+    for (
+        let x = start.x;
+        x != end.x + (start.x == end.x ? 1 : 0);
+        x += start.x > end.x ? -1 : 1
+    ) {
+        for (
+            let y = start.y;
+            y != end.y + (start.y == end.y ? 1 : 0);
+            y += start.y > end.y ? -1 : 1
+        ) {
+            grid[x][y] += 1;
+        }
+    }
+
+    grid[end.x][end.y] += 1;
+});
+
+let output = "";
+let intersections = 0;
+
+grid.forEach((row) => {
+    row.forEach((number) => {
+        output += number;
+
+        if (number > 1) {
+            intersections++;
+            // console.log(number);
+        }
+    });
+    output += "\n";
+});
+
+console.log(intersections);
+fs.writeFileSync("./grid.txt", output);
